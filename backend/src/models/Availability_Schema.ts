bb195df0384242b6eb0539ef0a8f2fda0caad842
nithin441122 <nithinreddy441122@gmail.com>
2025-01-21 15:31:42 +0530

    Deleted unnecessary files and cleaned up the repository

--- backend/src/models/Availability_Schema.ts
+++
@@ -1,5 +0,0 @@
-import { Schema } from "mongoose";
-
-export const AvailabilitySchema = new Schema({
-  availableFrom: { type: Date, required: true },
-});
--- backend/src/models/Property_Identification_Schema.ts
+++
@@ -1,6 +0,0 @@
-import { Schema } from "mongoose";
-
-export const PropertyIdentificationSchema = new Schema({
-  propertyId: { type: String, required: true },
-  leaseNo: { type: String, required: false },
-});
