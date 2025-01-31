eca38eb5a0be4bf42b0715fe45fa9cdded5a9789
manasabukkacherla <214g1a0555@srit.ac.in>
2025-01-31 14:18:49 +0530

    login and signup code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,6 +24,7 @@ import enquiryRoutes from "./routes/enquiryRoutes";
 import signupRouter from "./routes/signupform";// Replace with the correct file path for User
 import loginRouter from "./routes/authRoutes";
 import forgotPasswordRoutes from "./routes/forgotPasswordRoutes";
+
 dotenv.config();
 
 // Validate required environment variables
@@ -66,6 +67,7 @@ app.use("/api/service", enquiryRoutes);
 app.use("/api/sign",signupRouter);
 app.use("/api/auth", loginRouter);
 app.use("/api", forgotPasswordRoutes); 
+
 // Basic route
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the API" });
