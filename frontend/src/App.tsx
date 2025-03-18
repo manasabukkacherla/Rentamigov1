f054f7f366b08f4a0657f06c17383793bcadfb9f
manasabukkacherla <214g1a0555@srit.ac.in>
2025-03-18 13:36:49 +0530

    tokensplans are added in the usrdashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -70,7 +70,7 @@ const App = () => {
           <Route path="/owner-page" element={<OwnerPage />} />
           <Route path="/service-page" element={<Services />} />
 
-          <Route path="/Userdashboard/" element={<UsrDashboard />} />
+          <Route path="/Userdashboard/*" element={<UsrDashboard />} />
 
           {/* Property and Map Pages */}
           <Route path="/Fullpage/:id" element={<Fullpage />} />
